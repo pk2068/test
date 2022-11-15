4e1738c9d06e90c1cbe1e50e3f2ca4c8b7b3050d
pk2068 <36703153+pk2068@users.noreply.github.com>
2022-11-15 14:48:42 +0100

    Clean FABRIC theme view

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import dataA from './dataSource.json';
 import dataB from './dataSource2.json';
 import { React, useState } from "react";
 //import { ColumnsDirective, ColumnDirective, Filter } from "@syncfusion/ej2-react-grids";
-import {   ColumnDirective, ColumnsDirective, Inject, Filter,
-  Sort, Resize, VirtualScroll, InfiniteScroll, ContextMenu
+import {
+     ColumnDirective, ColumnsDirective, Filter,
+     Sort, Resize, VirtualScroll, Inject, InfiniteScroll, ContextMenu, GridComponent
 } from '@syncfusion/ej2-react-grids';
-import { UniversalGrid, DefaultSettings  } from "@mscrmaddons/components";
+
 import { DefaultButton } from "@fluentui/react";
 import { registerLicense } from '@syncfusion/ej2-base';
 
@@ -15,95 +16,126 @@ registerLicense('ORg4AjUWIQA/Gnt2VVhjQlFac11JXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXx
 
 function App() {
 
-  const [data, setData] = useState(() => dataA);
-
-  const changeData = (e) => {
-    if (data.length === dataA.length)
-      setData(dataB)
-    else
-      setData(dataA);
-  }
-
-  const columnProps =
-                    (
-                        <ColumnsDirective>
-                            <ColumnDirective field='id' headerText='ID' textAlign='Left' width={50} isPrimaryKey={true} clipMode='EllipsisWithTooltip' />
-                            <ColumnDirective field='serviceName' headerText='Name' textAlign='Left' width={150} clipMode='EllipsisWithTooltip'
-                                 />
-                            <ColumnDirective field='userName' headerText='User' textAlign='Left' width={200} clipMode='EllipsisWithTooltip' a />
-                            <ColumnDirective field='createdon' headerText='Created' textAlign='Left' width={70}
-                                 />
-                            <ColumnDirective field='organizationName' headerText='Organization' textAlign='Left' width={150} />
-                            <ColumnDirective field='instanceName' headerText='Instance' textAlign='Left' width={100} clipMode='EllipsisWithTooltip' />
-                            <ColumnDirective field='ServiceStopDate' type="date" headerText='Stop Date' textAlign='Left' width={100} clipMode='EllipsisWithTooltip'
-                                 />
-                            <ColumnDirective field='discoveryWebServiceUrl' headerText='Discovery Web Service Url' textAlign='Left' width={350} clipMode='EllipsisWithTooltip' />
-                            <ColumnDirective field='ServiceStatusDescription' headerText='Status Description' textAlign='Left' width={250} clipMode='EllipsisWithTooltip' allowResizing={true} />
-                            <ColumnDirective field='inactiveSince' type="date" headerText='Inactive Since' textAlign='Center' width={70}
-                                 clipMode='EllipsisWithTooltip' />
-                            <ColumnDirective field='licenseKey' textAlign='Center'
-                                headerText='License Key' width={100} clipMode='EllipsisWithTooltip'                                
-                                headerTextAlign='Left'
-                                filter={{ type: 'CheckBox' }} />
-
-                            <ColumnDirective field='subscriptionNumber' headerText='SubscriptionNumber' textAlign='Center' width={150}
-                                 />
-                            <ColumnDirective field='validDate' type="date" headerText='Expired(valid)' textAlign='Center' width={100}
-                                 />
-                            <ColumnDirective field='purchased' headerText='Purchased' textAlign='Center' width={80} clipMode="EllipsisWithTooltip" />
-                            <ColumnDirective field='count' headerText='Site Licenses' textAlign='Center' width={150} clipMode="EllipsisWithTooltip" />
-                            <ColumnDirective field='crmVersion' headerText='CRM Version' clipMode='EllipsisWithTooltip'
-                                textAlign='Left' width={100}  />
-                            <ColumnDirective field='sendChanges' headerText='Send Changes' textAlign='Center' width={70}
-                                 clipMode='EllipsisWithTooltip' />
-                            <ColumnDirective field='disabled' headerText='disabled' textAlign='Center' width={70}
-                                 clipMode='EllipsisWithTooltip' />
-
-                        </ColumnsDirective>
-                    );
-
-  // let mysett = { ...DefaultSettings };
-  // mysett.allowSorting = true;
-  // mysett.enableSorting = true;
-  // mysett.enableFilter = true;
-  // mysett.enableSearch = false;
-  
+     const [data, setData] = useState(() => dataA);
 
 
-  return (
-    <div className="App">
-      <p>Testing the new UniversalGrid component</p>
-      <h1>{dataA.length}</h1>
-      <h1>{dataB.length}</h1>
-      <p>Trigger Changes</p>
-      <DefaultButton text={"Change Data"} onClick={changeData} /> 
-      {/* <UniversalGrid debug={true} dataSource={data} /> */}
+     const changeData = (e) => {
+          if (data.length === dataA.length)
+               setData(dataB)
+          else
+               setData(dataA);
+     }
 
-      <UniversalGrid debug={true}  dataSource={data} 
-      injections={[Filter, Sort, VirtualScroll, Resize, InfiniteScroll, ContextMenu] }  
-      columns={columnProps}/>
-      
-      
 
-    </div>
+     //#region SyncFS GridComponent settings
+     const settings = { type: 'Multiple', mode: 'Both' };
+     const filterOptions = {
+          mode: 'Immediate',
+          ignoreAccent: true,
+          showFilterBarOperator: true,
+          type: 'Menu'
+     }
+     const contextMenuItems = [
+          { text: 'Open Config', id: 'openconfig1', target: '.e-content' },
+          { text: 'Open Config as Admin', id: 'openconfig2', target: '.e-content' },
+          { text: 'Open Config as Admin filtered', id: 'openconfig3', target: '.e-content' }
+      ];
+     const contextmenuClick = async (ev) => { }
 
-  );
+          
+     //#endregion
 
-  //const [value, SetValue] = useState(initialState)
+     return (
+          <div className="App">
+               <p>Testing the GridComponent theme</p>
+               
+               <p>Trigger Changes</p>
+               <DefaultButton text={"Change Data"} onClick={changeData} />
+               
 
-  //     const [data, setData] = useState(data1);
 
-  //     return (        <div className="App">            <p>Testing the new UniversalGrid component</p>            <h1>{data1.length}</h1>            <h1>{data2.length}</h1>            <p>Trigger Changes</p>
+               <GridComponent
+                    dataSource={data}
+                    enableInfiniteScrolling={true}
+                    enableVirtualization={false}
+                    enableHeaderFocus={false}
+                    enableSearch={false}
+                    height={'60vh'}
+                    rowHeight={30}
+                    allowSelection={true}
+                    allowFiltering={true}
+                    filterSettings={filterOptions}
+                    allowPaging={false}                     
+                    allowResizing={true}
+                    allowReordering={true}
+                    allowSorting={true}
+                    allowGrouping={false}
+                    delayUpdate={true}
+                    selectionSettings={settings}
+                    rowSelected={ (e) => {} }
+                    contextMenuItems={contextMenuItems}
+                    contextMenuClick={contextmenuClick}
+               >
+                    <ColumnsDirective>
+                         <ColumnDirective field='id' headerText='ID' textAlign='Left' width={50} isPrimaryKey={true} clipMode='EllipsisWithTooltip' />
+                         <ColumnDirective field='serviceName' headerText='Name' textAlign='Left' width={150} clipMode='EllipsisWithTooltip'
+                         />
+                         <ColumnDirective field='userName' headerText='User' textAlign='Left' width={200} clipMode='EllipsisWithTooltip' a />
+                         <ColumnDirective field='createdon' headerText='Created' textAlign='Left' width={70}
+                         />
+                         <ColumnDirective field='organizationName' headerText='Organization' textAlign='Left' width={150} />
+                         <ColumnDirective field='instanceName' headerText='Instance' textAlign='Left' width={100} clipMode='EllipsisWithTooltip' />
+                         <ColumnDirective field='ServiceStopDate' type="date" headerText='Stop Date' textAlign='Left' width={100} clipMode='EllipsisWithTooltip'
+                         />
+                         <ColumnDirective field='discoveryWebServiceUrl' headerText='Discovery Web Service Url' textAlign='Left' width={350} clipMode='EllipsisWithTooltip' />
+                         <ColumnDirective field='ServiceStatusDescription' headerText='Status Description' textAlign='Left' width={250} clipMode='EllipsisWithTooltip' allowResizing={true} />
+                         <ColumnDirective field='inactiveSince' type="date" headerText='Inactive Since' textAlign='Center' width={70}
+                              clipMode='EllipsisWithTooltip' />
+                         <ColumnDirective field='licenseKey' textAlign='Center'
+                              headerText='License Key' width={100} clipMode='EllipsisWithTooltip'
+                              headerTextAlign='Left'
+                              filter={{ type: 'CheckBox' }} />
 
-  //             <DefaultButton text={"Update DS"} onClick={() => setData(data2)}/>
+                         <ColumnDirective field='subscriptionNumber' headerText='SubscriptionNumber' textAlign='Center' width={150}
+                         />
+                         <ColumnDirective field='validDate' type="date" headerText='Expired(valid)' textAlign='Center' width={100}
+                         />
+                         <ColumnDirective field='purchased' headerText='Purchased' textAlign='Center' width={80} clipMode="EllipsisWithTooltip" />
+                         <ColumnDirective field='count' headerText='Site Licenses' textAlign='Center' width={150} clipMode="EllipsisWithTooltip" />
+                         <ColumnDirective field='crmVersion' headerText='CRM Version' clipMode='EllipsisWithTooltip'
+                              textAlign='Left' width={100} />
+                         <ColumnDirective field='sendChanges' headerText='Send Changes' textAlign='Center' width={70}
+                              clipMode='EllipsisWithTooltip' />
+                         <ColumnDirective field='disabled' headerText='disabled' textAlign='Center' width={70}
+                              clipMode='EllipsisWithTooltip' />
 
-  //             <UniversalGrid debug={true}  dataSource={data} injections={[Filter]} columns={(
+                    </ColumnsDirective>
+                    <Inject services={[Filter, Sort, VirtualScroll, Resize, InfiniteScroll, ContextMenu]} />
+               </GridComponent>
 
-  //                 <ColumnsDirective>                    <ColumnDirective field={"id"}/>                    <ColumnDirective field={"serviceName"}/>                    <ColumnDirective field={"instanceName"}/>                </ColumnsDirective>
+               <h1>Num of rows in data source: {data.length}</h1>
 
-  //             )}/>
 
-  //         </div>    );
+
+          </div>
+
+     );
+
+     //const [value, SetValue] = useState(initialState)
+
+     //     const [data, setData] = useState(data1);
+
+     //     return (        <div className="App">            <p>Testing the new UniversalGrid component</p>            <h1>{data1.length}</h1>            <h1>{data2.length}</h1>            <p>Trigger Changes</p>
+
+     //             <DefaultButton text={"Update DS"} onClick={() => setData(data2)}/>
+
+     //             <UniversalGrid debug={true}  dataSource={data} injections={[Filter]} columns={(
+
+     //                 <ColumnsDirective>                    <ColumnDirective field={"id"}/>                    <ColumnDirective field={"serviceName"}/>                    <ColumnDirective field={"instanceName"}/>                </ColumnsDirective>
+
+     //             )}/>
+
+     //         </div>    );
 }
 
 export default App;
